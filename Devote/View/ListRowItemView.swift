1b67d3111795cbc27c1990b1dbc4bfd8e6d48ae4
David Owen <owend91@gmail.com>
2022-12-12 11:06:02 -0700

    Add missing files

---
+++ Devote/View/ListRowItemView.swift
@@ -0,0 +1,32 @@
+//
+//  ListRowItemView.swift
+//  Devote
+//
+//  Created by David Owen on 12/12/22.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? .pink : .primary)
+                .padding(.vertical, 12)
+                .animation(.default)
+        }
+        .toggleStyle(CheckboxStyle())
+        .onReceive(item.objectWillChange) { _ in
+            if self.viewContext.hasChanges {
+                try? self.viewContext.save()
+            }
+        }
+    }
+}
+
+
